e694fbd295bd50751b51de423e0a36796c806519
MD-ISHAM <113245934+ISHAM-GITS@users.noreply.github.com>
2024-12-29 18:54:50 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,6 @@
+function menu(){
+    const menuu=document.querySelector(".menu-links");
+    const icon=document.querySelector(".nav-icon");
+menuu.classList.toggle("open");
+icon.classList.toggle("open");
+}
